d4eca2e423e55e1354983f9c169b795e01890204
Odriver <519968509@qq.com>
2026-05-25 11:19:12 +0800

    Delete WebGal directory

--- WebGal/src/Core/Modules/danmaku/index.ts
+++
@@ -1,6 +0,0 @@
-export { DanmakuCore } from './DanmakuCore';
-export { DanmakuManager } from './DanmakuManager';
-export { DanmakuApi } from './DanmakuApi';
-export type { DanmakuDataMap, DanmakuRawItem } from './DanmakuApi';
-export { danmakuIsValidColor, danmakuNormalizeColor, DANMAKU_COLOR_PRESETS, DANMAKU_DEFAULTS } from './DanmakuConfig';
-export type { DanmakuServerConfig } from './DanmakuConfig';
